b8e4472ea86c6534d20563dae0514042d2bf4c83
Prajna Patel <153821747+Prajna-patel@users.noreply.github.com>
2025-03-25 11:27:22 +0530

    Delete matrimony/matriapp directory

--- matrimony/matriapp/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class MatriappConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'matriapp'
-
-    def ready(self):
-        import matriapp.signals  # Import signals to ensure they are registered
